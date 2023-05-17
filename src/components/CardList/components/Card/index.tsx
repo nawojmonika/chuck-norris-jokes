10701ae0cacb20408c079a563ff77554ee484d02
nawojmonika <nawojmonika@gmail.com>
2023-05-17 21:59:59 +0200

    added basic card styling

--- src/components/CardList/components/Card/index.tsx
+++ src/components/CardList/components/Card/index.tsx
@@ -1,7 +1,8 @@
 import { CardItem } from '../../../../api';
+import styles from './Card.module.css';
 
 type Props = CardItem;
 
 export const Card = ({ id, value }: Props) => {
-	return <div>{value}</div>;
+	return <div className={styles.card}>{value}</div>;
 };
