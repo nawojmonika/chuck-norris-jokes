96baacf455561e1e75f06eae5510463bdf7786e5
nawojmonika <nawojmonika@gmail.com>
2023-05-17 23:06:38 +0200

    Added updating joke list

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App(): JSX.Element {
 
 	useEffect(() => {
 		let ignore = false;
+
 		if (!ignore) {
 			fetchJoke(10).then((result) => {
 				setJokes(result);
@@ -18,6 +19,27 @@ function App(): JSX.Element {
 			ignore = true;
 		};
 	}, []);
+
+	useEffect(() => {
+		let timeoutId: any;
+
+		if (!timeoutId) {
+			timeoutId = setTimeout(() => {
+				fetchJoke().then((result) => {
+					console.log('refresh!');
+					const temp = [...jokes];
+					temp.pop();
+					temp.unshift(result[0]);
+					setJokes(temp);
+				});
+			}, 5000);
+		}
+
+		return () => {
+			clearTimeout(timeoutId);
+			timeoutId = null;
+		};
+	}, [jokes]);
 
 	return <CardList list={jokes} />;
 }
