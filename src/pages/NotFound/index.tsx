1dd4832051a8c85bb60f0fe20b3366dd9eb7da41
nawojmonika <nawojmonika@gmail.com>
2023-05-18 18:01:34 +0200

    updated title of 404 page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -3,7 +3,7 @@ import sadChuck from '../../assets/sad_chuck.png';
 export const NotFound = (): JSX.Element => {
 	return (
 		<>
-			<h3>Jokes on you, this page does not exist!</h3>
+			<h3>Jokes on you, the page you are looking for does not exist!</h3>
 			<img src={sadChuck} alt='Sad Chuck image' aria-hidden={true} />
 		</>
 	);
