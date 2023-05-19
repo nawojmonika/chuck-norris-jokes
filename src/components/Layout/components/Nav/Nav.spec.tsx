3963522a89d947146cb71b7fe8fb742e27b49d9c
nawojmonika <nawojmonika@gmail.com>
2023-05-19 13:53:10 +0200

    Added Nav component unit tests

---
+++ src/components/Layout/components/Nav/Nav.spec.tsx
@@ -0,0 +1,42 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import { Nav } from './index';
+
+const setUp = (): void => {
+	render(
+		<Router>
+			<Routes>
+				<Route
+					path='/'
+					element={
+						<>
+							<Nav />
+						</>
+					}>
+					<Route index element={<></>} />
+					<Route path='favorites' element={<></>} />
+				</Route>
+			</Routes>
+		</Router>
+	);
+};
+
+describe('Nav component', () => {
+	test('Should render Page title', () => {
+		setUp();
+		const title = screen.getByText('Chuck Norris jokes');
+		expect(title).toBeVisible();
+	});
+	test('Should render Home page link', () => {
+		setUp();
+		const homeLink = screen.getByText('Home');
+		expect(homeLink).toBeVisible();
+		expect(homeLink).toHaveAttribute('href', '/');
+	});
+	test('Should render Favorites page link', () => {
+		setUp();
+		const favoritesLink = screen.getByText('Favorites');
+		expect(favoritesLink).toBeVisible();
+		expect(favoritesLink).toHaveAttribute('href', '/favorites');
+	});
+});
